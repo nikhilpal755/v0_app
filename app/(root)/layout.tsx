f6835207d779f8e320627b1b456af463f17066bc
nikhil <nikhil16175@gmail.com>
2026-01-15 15:47:07 +0530

    feat: added supabase theme and navbar changes

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,5 @@
 import { onBoardUser } from '@/modules/auth/actions'
+import Navbar from '@/modules/home/components/navbar';
 import React from 'react'
 
 export default async function layout({ children }: React.PropsWithChildren) {
@@ -6,8 +7,9 @@ export default async function layout({ children }: React.PropsWithChildren) {
     return (
         <main className='flex flex-col min-h-screen relative overflow-x-hidden'>
             {/* navbar */}
+            <Navbar />
             <div className='fixed inset-0 -z-10 h-full w-full bg-background dark:bg-[radial-gradient(#393e4a_1px,transparent_1px)] bg-[radial-gradient(#dadde2_1px,transparent_1px)] [background-size: 16px_16px]'></div>
-            <div className='flex-1 w-full mt-20'>
+            <div className='flex-1 w-full'>
                 {children}
 
             </div>
